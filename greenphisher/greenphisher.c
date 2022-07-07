97cc2d045ce57c42c079a48a5ce3586bc7ff4adc
MoryZoe1 <90766106+MoryZoe1@users.noreply.github.com>
2022-07-07 18:37:08 +0530

    Add files via upload

---
+++ greenphisher/greenphisher.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include <dirent.h> 
+#include <stdio.h> 
+void main(){
+    DIR *d;
+  struct dirent *dir;
+  d = opendir(".");
+  if (d) {
+    while ((dir = readdir(d)) != NULL) {
+      printf("%s\n", dir->d_name);
+    }
+    closedir(d);
+  }
+}
